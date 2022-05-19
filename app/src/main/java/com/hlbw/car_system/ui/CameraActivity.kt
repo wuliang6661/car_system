e3ae661f969f03fa2445bea152a0288bc829e8a3
wuliang6661@163.com <wy19941007>
2022-05-19 11:13:18 +0800

    1111

--- app/src/main/java/com/hlbw/car_system/ui/CameraActivity.kt
+++ app/src/main/java/com/hlbw/car_system/ui/CameraActivity.kt
@@ -23,16 +23,15 @@ class CameraActivity : BaseActivity() {
 
     private var preview: Preview? = null
     private var imageCapture: ImageCapture? = null
-    private var imageAnalyzer: ImageAnalysis? = null
     private var camera: Camera? = null
 
     private lateinit var outputDirectory: File
     private lateinit var cameraExecutor: ExecutorService
 
-    private val takePhoto : View by lazy {
+    private val takePhoto: View by lazy {
         findViewById(R.id.take_photo)
     }
-    private val viewFinder : PreviewView by lazy {
+    private val viewFinder: PreviewView by lazy {
         findViewById(R.id.viewFinder)
     }
 
@@ -47,79 +46,68 @@ class CameraActivity : BaseActivity() {
         findViewById<View>(R.id.back).setOnClickListener {
             finish()
         }
-
         // Request camera permissions
         startCamera()
-
         // Setup the listener for take photo button
         takePhoto.setOnClickListener { takePhoto() }
-
         outputDirectory = getOutputDirectory()
-
         cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
 
     private fun startCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
-
-        cameraProviderFuture.addListener(Runnable {
-            // Used to bind the lifecycle of cameras to the lifecycle owner
-            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-            // Preview
-            preview = Preview.Builder().build()
-            // Select back camera
-            val cameraSelector = CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
-            try {
-                // Unbind use cases before rebinding
-                cameraProvider.unbindAll()
-                // Bind use cases to camera
-                camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+        imageCapture = ImageCapture.Builder().build()
+        cameraProviderFuture.addListener({ // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get() // Preview
+            preview = Preview.Builder().build() //创建图片的 capture
+            imageCapture = ImageCapture.Builder().setFlashMode(ImageCapture.FLASH_MODE_OFF)
+                .build() // Select back camera
+            val cameraSelector =
+                CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+            try { // Unbind use cases before rebinding
+                cameraProvider.unbindAll() // Bind use cases to camera
+                camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
                 preview?.setSurfaceProvider(viewFinder.createSurfaceProvider(camera?.cameraInfo))
-            } catch(exc: Exception) {
+            } catch (exc: Exception) {
                 Log.e(TAG, "Use case binding failed", exc)
             }
         }, ContextCompat.getMainExecutor(this))
     }
 
 
-    private fun takePhoto() {
-        // Get a stable reference of the modifiable image capture use case
+    private fun takePhoto() { // Get a stable reference of the modifiable image capture use case
         val imageCapture = imageCapture ?: return
-
         // Create timestamped output file to hold the image
-        val photoFile = File(
-            outputDirectory,
-            SimpleDateFormat(FILENAME_FORMAT, Locale.US
-            ).format(System.currentTimeMillis()) + ".jpg")
-
+        val photoFile = File(outputDirectory,
+                             SimpleDateFormat(FILENAME_FORMAT,
+                                              Locale.US).format(System.currentTimeMillis()) + ".jpg")
         // Create output options object which contains file + metadata
         val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
-
         // Setup image capture listener which is triggered after photo has
         // been taken
-        imageCapture.takePicture(
-            outputOptions, ContextCompat.getMainExecutor(this), object : ImageCapture.OnImageSavedCallback {
-                override fun onError(exc: ImageCaptureException) {
-                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
-                }
+        imageCapture.takePicture(outputOptions,
+                                 ContextCompat.getMainExecutor(this),
+                                 object : ImageCapture.OnImageSavedCallback {
+                                     override fun onError(exc: ImageCaptureException) {
+                                         Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                                     }
 
-                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
-                    val savedUri = Uri.fromFile(photoFile)
-                    val msg = "Photo capture succeeded: $savedUri"
-                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-                    Log.d(TAG, msg)
-                }
-            })
+                                     override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                                         val savedUri = Uri.fromFile(photoFile)
+                                         val msg = "Photo capture succeeded: $savedUri"
+                                         Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                                         Log.d(TAG, msg)
+                                     }
+                                 })
     }
-
 
 
     private fun getOutputDirectory(): File {
         val mediaDir = externalMediaDirs.firstOrNull()?.let {
-            File(it, resources.getString(R.string.app_name)).apply { mkdirs() } }
-        return if (mediaDir != null && mediaDir.exists())
-            mediaDir else filesDir
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
+        }
+        return if (mediaDir != null && mediaDir.exists()) mediaDir else filesDir
     }
 
     companion object {
